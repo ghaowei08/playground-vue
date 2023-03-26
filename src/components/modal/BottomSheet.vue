ee5913d7d208b797f39e799e7188db942c60c09f
Haowei Goh <43960083+ghaowei08@users.noreply.github.com>
2023-03-26 22:37:58 +0800

    modal & transition

---
+++ src/components/modal/BottomSheet.vue
@@ -0,0 +1,50 @@
+<template>
+  <div style="position: absolute; top: 0; left: 0; z-index: 50; overflow: hidden;">
+    <Transition name="modal">
+      <div v-if="isOpen" style="width: 100vw; height: 100vh; background-color: red;">
+        <button @click="hide">hide</button>
+        this is a modal
+      </div>
+    </Transition>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, } from 'vue';
+
+const isOpen = ref(false)
+
+const show = () => {
+  isOpen.value = true
+}
+
+const hide = () => {
+  isOpen.value = false
+}
+
+defineExpose({ show })
+
+</script>
+
+<style>
+.modal-enter-active {
+  transition: 2s ease-in;
+}
+
+.modal-leave-active {
+  transition: 2s ease-out;
+}
+
+.modal-enter-from,
+.modal-leave-to {
+  transition-property: all;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 150ms;
+  transform: translateY(100%);
+}
+
+.modal-enter-to,
+.modal-leave-from {
+  transform: translateY(0px);
+}
+</style>
---
+++ src/views/ModalView.vue
@@ -0,0 +1,32 @@
+<template>
+  <button @click="selectedComponent.action">Toggle</button>
+  <component :is="selectedComponent.component" :ref="selectedComponent.ref"></component>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import BottomSheet from '../components/modal/BottomSheet.vue'
+
+const bottomSheet = ref<InstanceType<typeof BottomSheet>>()
+
+const selectedComponent = computed(() => ({
+  component: BottomSheet,
+  ref: bottomSheet,
+  action: () => {
+    bottomSheet.value?.show()
+  }
+}))
+</script>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.v-enter-from,
+.v-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+</style>
